ec54c0f3c703d0ae571cdb8ca35d146b23053688
Mari Kujiraoka <ciaociao13ema@gmail.com>
2026-04-10 19:28:37 +0900

    I18nize CheckTreasureEmeraldCommand with zero hardcoded messages

--- src/main/java/plugin/CheckTreasureEmeraldCommand.java
+++ src/main/java/plugin/CheckTreasureEmeraldCommand.java
@@ -10,22 +10,32 @@ import org.bukkit.inventory.ItemStack;
 public class CheckTreasureEmeraldCommand implements CommandExecutor {
 
   private final TreasureRunMultiChestPlugin plugin;
+  private final I18nHelper i18n;
 
   public CheckTreasureEmeraldCommand(TreasureRunMultiChestPlugin plugin) {
     this.plugin = plugin;
+    this.i18n = new I18nHelper(plugin);
   }
 
   @Override
   public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
     if (!(sender instanceof Player player)) {
-      sender.sendMessage("このコマンドはプレイヤーのみが実行できます。");
+      sender.sendMessage(ChatColor.RED + i18n.trDefault(
+          "command.checkTreasureEmerald.playersOnly"
+      ));
       return true;
     }
 
     ItemStack item = player.getInventory().getItemInMainHand();
     boolean result = plugin.getItemFactory().isTreasureEmerald(item);
 
-    player.sendMessage(ChatColor.AQUA + "isTreasureEmerald = " + (result ? ChatColor.GREEN + "true" : ChatColor.RED + "false"));
+    player.sendMessage(ChatColor.AQUA + i18n.tr(
+        player,
+        result
+            ? "command.checkTreasureEmerald.resultTrue"
+            : "command.checkTreasureEmerald.resultFalse"
+    ));
+
     return true;
   }
 }
